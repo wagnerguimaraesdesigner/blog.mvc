576cc72a55765db9b06a3710668024ec617a9fdc
WagnerGuimaraes <wagnerguimaraesdesigner@gmail.com>
2025-08-13 22:35:48 -0300

    Criando Postagens 13/08

--- BlogBea/Controllers/HomeController.cs
+++ BlogBea/Controllers/HomeController.cs
@@ -15,30 +15,71 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        ViewData["Mensagem"] = "Oi mundo!";
-        Categoria categoria = new ();
-        categoria.Id = 1;
-        categoria.Nome = "Tegnologia";
+        Categoria tecnologia = new ();
+        tecnologia.Id = 1;
+        tecnologia.Nome = "Tegnologia";
         Categoria categoria2 =  new(){
             Id= 2,
             Nome = "IA"
         };
 
+        Categoria receita = new(3, "Morango do amor");
+
         List<Postagem> postagens =[
             new(){
                 Id = 1,
-                Nome = "Saiu o ChatGpt 5",
+                Nome = "Receita Morango do Amor",
                 CategoriaId = 2,
                 Categoria = categoria2,
                 DataPostagem = DateTime.Parse("08/08/2025"),
                 Descricao = "dfçlkgjmçldfg",
-                Texto = "kgklfghlkfghj"
+                Texto = "kgklfghlkfghj",
+                Thumbnail = "/img/1.jpeg",
+                Foto = "/img/1.jpeg"
 
-            }
+            },
+
+              new(){
+                Id = 2,
+                Nome = "Receita Morango do Amor 2",
+                CategoriaId = 3,
+                Categoria = receita,
+                DataPostagem = DateTime.Parse("08/08/2025"),
+                Descricao = "teste",
+                Texto = "testeeee",
+                Thumbnail = "/img/1.jpeg",
+                Foto = "/img/1.jpeg"
+
+            },
+
+                 new(){
+                Id = 3,
+                Nome = "Receita Morango do Amor 3",
+                CategoriaId = 4,
+                Categoria = receita,
+                DataPostagem = DateTime.Parse("08/08/2025"),
+                Descricao = "teste",
+                Texto = "testeeee",
+                Thumbnail = "/img/1.jpeg",
+                Foto = "/img/1.jpeg"
+
+            },
+                 new(){
+                Id = 4,
+                Nome = "Receita Morango do Amor 4",
+                CategoriaId = 5,
+                Categoria = receita,
+                DataPostagem = DateTime.Parse("08/08/2025"),
+                Descricao = "teste",
+                Texto = "testeeee",
+                Thumbnail = "/img/1.jpeg",
+                Foto = "/img/1.jpeg"
+
+            },
         ];
 
 
-        return View();
+        return View(postagens);
     }
 
     public IActionResult Privacy()
--- BlogBea/Models/Categoria.cs
+++ BlogBea/Models/Categoria.cs
@@ -6,7 +6,18 @@ public class Categoria
 {
     public int Id { get; set; }
 
+    
+
     [Required]
     [StringLength(30)]
     public string Nome { get; set; }
+
+    public Categoria() {}
+
+    public Categoria(int id, string nome)
+    {
+        Id = id;
+        Nome = nome;
+    }
 }
+ 
